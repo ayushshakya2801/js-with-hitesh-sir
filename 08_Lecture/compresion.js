e8a578051e029a5a0b02362ef6ddd14802071fcc
Ayush <shakyaayush31@gmail.com>
2024-12-11 23:27:50 +0530

    comprasion of datatype in js

---
+++ 08_Lecture/compresion.js
@@ -0,0 +1,24 @@
+
+//  comprasion of datatype in js 
+
+//  console.log(8>6); true
+//  console.log(6>9); false
+//  console.log(7==7); true
+//  console.log(5!=7); true
+//  console.log(5>=7); false
+//  console.log(5<=7);  true
+
+console.log(null > 0); false
+console.log(null == 0); false
+console.log(null >= 0); true
+ console.log(undefined == 0);  false
+
+  // ===  
+
+//   3 equal mai yai pehle check karega ki yai equal hai fir check karega ki inka data type same hai dono number ya dono string ya fir koe bhi ho pr data type same hona chaiya 
+ 
+
+ console.log("7"===7)  // in case he show false 
+ console.log("7" === "7"); // in this he show true bcz its data types are same both are boolean
+
+ 
