9314425ca1861b94abdece59276d3dc679a812be
Ayush <shakyaayush31@gmail.com>
2024-12-12 00:47:52 +0530

    data types

---
+++ 09_Lecture/Datatypes.js
@@ -0,0 +1,33 @@
+//   Data types in JS..
+
+ // Data types divided into 2 parts 
+
+ // 1. Permitive
+  
+// string , boolearn , number , undefined , null , BigInt , symbol.
+
+
+// 2.  Non permitive 
+    //   Array , Objects , Function
+
+//    console.log(typeof 42); // "number"
+// console.log(typeof "hello"); // "string"
+// console.log(typeof null); // "object" (a well-known quirk)
+// console.log(typeof undefined); // "undefined"
+
+
+//  const myFunction = function () {
+
+//      console.log("hellow words");
+//     }
+//     console.log(typeof myFunction)
+
+    // ans is Function 
+
+      const heros = [ "Ayush", "Suresh", "patel"];  // object
+      let myObj ={
+         age : 52, // undefined
+          Name : "Hitesh", // undefined
+      }
+
+      console.log(typeof age)
