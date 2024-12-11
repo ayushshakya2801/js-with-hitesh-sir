666f08f7adf2c94082eedc1a81f605cad674cd42
Ayush <shakyaayush31@gmail.com>
2024-12-11 22:58:55 +0530

    opreator

---
+++ 7_lecture/Opreator.js
@@ -0,0 +1,42 @@
+
+
+// Opreators in js  
+
+//  let num = 53;
+//  let negNum = -num
+//     console.log(negNum)
+
+//   he gives in negative value of output -53
+
+    // console.log( 7+9)
+    // console.log(8-5);
+    // console.log(8*2);
+    // console.log(8/2);
+    // console.log(9%2); 
+    //  yai dekhata hai ki maine apne number ki divide kiya hai to jo remeber bacha hai bo he show karega yai eshme 9 ko 2 se dive kiya ko remenber 1 bach ahai 
+    
+    
+    //  add string 
+
+//  let name1 = "Ayush"
+//  let name2 = " shakay"
+// let  fullName = name1 + name2
+// console.log(fullName);
+
+ 
+// ++variable; // Pre-increment
+// variable++; // Post-increment
+ 
+//  pre -   increament opretor 
+// let x = 5;
+// let y = ++x; // Increment x first, then assign to y
+// console.log(x); // 6
+// console.log(y);  // 6
+
+// post - increament 
+let x = 5;
+let y = x++; // Assign the value of x to y, then increment x
+console.log(y); 5
+console.log(x); 6
+  
+   
