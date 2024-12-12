b112de94d2e24e9a30c3632f08739d9825f8130e
Ayush <shakyaayush31@gmail.com>
2024-12-12 10:47:44 +0530

    strings

---
+++ 11_Lecture/strings.js
@@ -0,0 +1,20 @@
+
+// ++++++++++++++++
+ // strings
+
+//    const name = "Ayush"
+//    const city ="Noida"
+//     const id = 521;
+
+//     console.log(` My name is ${name} i am from ${city} and my std id is ${id}`);   
+    
+    // this is the professional lway to print all values
+
+   const name =  "ayush shakya"
+  const emailid = "ayushshakya@google.com"
+     console.log(name.toUpperCase());  // show his string in uppercase 
+     console.log(emailid.indexOf('y'))  // for my y kon se no par hai 
+     console.log(emailid.charAt(5))  // to check my email 5 no is which charector the alphaates is  s
+     console.log(emailid.length);   // count the lenght of email in this case the length is 22 
+     
+     
