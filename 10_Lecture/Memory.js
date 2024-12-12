1d7f97cb78ec435ffb0aae1a45c385fb9fcfb71e
Ayush <shakyaayush31@gmail.com>
2024-12-12 09:30:09 +0530

    memory lec 10

---
+++ 10_Lecture/Memory.js
@@ -0,0 +1,21 @@
+// +++++++++++++++++++++++
+ // Memory are 2 taypes in js 
+
+ // 1. stack     (All permitive data types ate hai )     stack mai hm jab bhi koe chij lege to hme ushak copy he milta hai
+ // 2.  heap   (All Non - permotive data taypes)    heap mai hme hamesa hme chij refrence se milti hai 
+
+//   let myYoutubename = "Ayush shakya"
+//    let  anothername = myYoutubename;
+//     mySecondYoutubename = "Chai aur code "
+//     console.log(mySecondYoutubename);
+//     console.log(anothername);
+    
+ 
+
+let myfirstYT = "Ayush shakya"
+let anothername = myfirstYT;
+
+secYT = "HTML Code"
+console.log(anothername);
+console.log(secYT);
+
