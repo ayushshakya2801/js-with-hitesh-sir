09712cbcbd00fc73226388ca0baf47906266d48d
Ayush <shakyaayush31@gmail.com>
2024-12-13 10:12:22 +0530

    math_random_no

---
+++ 12_Lecture/number_and_maths.js
@@ -0,0 +1,30 @@
+  // lecture no 12 number and maths
+
+// const score = 500;
+// console.log(score);
+
+// const newblance = new Number(90989);
+// console.log(newblance)
+// console.log(newblance.toString().length);
+
+ //+++++++++++++++++
+
+//   console.log(Math.random())  // Math.random no ganrate the random no under 0 to 1
+
+//   console.log(Math.abs(-6));     // - minus no ko plus kr deta hai 
+//   console.log(Math.round(4.6));
+//   console.log(Math.ceil(8.8));
+  
+  
+// Math.random number 
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);   // math . floor dekhata hai ki 5.89869 ushko ek single digit de deta hai
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min +1 )) + min )  
+ 
+// yha muje no 10 se 20 ke bech mai chaiye tha esh liya hamne +min no add kiya aur 
+ 
